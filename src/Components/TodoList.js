3bab1c7c2ea7efeb22c0df8a8f3918b02a3f4bee
MAS364 <siddiquiarshad106@gmail.com>
2023-09-27 00:46:05 +0530

    added media queries

--- src/Components/TodoList.js
+++ src/Components/TodoList.js
@@ -34,7 +34,7 @@ const TodosList = ({ todos, setTodos, setEditTodo }) => {
             className={`list ${todo.completed ? "complete" : ""}`}
             onChange={(event) => event.preventDefault()}
           />
-          <div>
+          <div className="btn">
             <button className="button-complete task-button" onClick={()=>handleComplete(todo)}>
               <i className="fa fa-check-circle"></i>
             </button>
